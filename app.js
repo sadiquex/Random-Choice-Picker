3f516da628b40cb2c233308b7e6bb25a331a726d
sadiquex <abubakasaddik1@gmail.com>
2022-07-16 20:56:53 +0000

    added tags functionality

---
+++ app.js
@@ -0,0 +1,13 @@
+// const tags = document.getElementById('tags');
+// const textArea = document.getElementById('textarea');
+
+// textArea.focus();
+
+// textArea.addEventListener('keyup', (e) => {
+//     createTags(e.target.value);
+// })
+
+// function createTags(input) {
+//     // use the split function to separate what user types by a comma into an array
+//     console.log(input)
+// }
